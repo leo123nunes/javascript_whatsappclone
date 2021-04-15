d48f9eb44e906c1b59704f4f49196c1966a98eae
leo123nunes <ti.userleo@hotmail.com>
2021-04-15 15:50:42 -0300

    Creating customized events in Element.prototype to make easy the code

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,6 +1,9 @@
 class WhatsAppController{
+
     constructor(){
         console.log('init whatsappcontroller...')
+
+        this.loadElementPrototypeCustomizedEvents()
 
         this.loadElements()
 
@@ -12,5 +15,52 @@ class WhatsAppController{
         document.querySelectorAll('[id]').forEach(element => {
             this.el[Format.getCamelCase(element.id)] = element
         })
+    }
+
+    loadElementPrototypeCustomizedEvents(){
+
+        Element.prototype.on = function(events, fn){
+            events.split(" ").forEach(event => {
+                this.addEventListener(event, fn)
+            })
+            return this
+        }
+
+        Element.prototype.show = function(){
+            this.style.display = "block"
+            return this
+        }
+
+        Element.prototype.hide = function(){
+            this.style.display = "none"
+            return this
+        }
+
+        Element.prototype.toggle = function(){
+            this.style.display = this.style.display == 'block' ? 'none' : 'block'
+            return this
+        }
+
+        Element.prototype.addClass = function(className){
+            this.classList.add(className)
+            return this
+        }
+
+        Element.prototype.removeClass = function(className){
+            this.classList.remove(className)
+            return this
+        }
+
+        Element.prototype.hasClass = function(className){
+            return this.hasClass(className)
+        }
+
+        Element.prototype.css = function(css){
+            Object.keys(css).forEach(styleName => {
+                this.style[styleName] = css[styleName]
+            })
+
+            return this
+        }
     }
 }

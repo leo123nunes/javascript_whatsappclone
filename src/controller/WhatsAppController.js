89808a8d8f6e5e767544847113a11116347b82b2
leo123nunes <ti.userleo@hotmail.com>
2021-04-16 15:34:50 -0300

    Adding events to the profile button and new conversation's button: open new panel when clicked

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -7,6 +7,8 @@ class WhatsAppController{
 
         this.loadElements()
 
+        this.initEvents()
+
         console.log(this.el)
     }
 
@@ -14,6 +16,36 @@ class WhatsAppController{
         this.el = {}
         document.querySelectorAll('[id]').forEach(element => {
             this.el[Format.getCamelCase(element.id)] = element
+        })
+    }
+
+    initEvents(){
+        this.el.myPhoto.on('click', event => {
+            this.el.panelEditProfile.show()
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open')
+            }, 300)
+        })
+
+        this.el.btnClosePanelEditProfile.on('click', event => {
+            this.el.panelEditProfile.removeClass('open')
+            setTimeout(() => {
+                this.el.panelEditProfile.hide()
+            }, 300)
+        })
+
+        this.el.btnNewContact.on('click', event => {
+            this.el.panelAddContact.show()
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open')
+            }, 300)
+        })
+
+        this.el.btnClosePanelAddContact.on('click', event => {
+            this.el.panelAddContact.removeClass('open')
+            setTimeout(() => {
+                this.el.panelAddContact.hide()
+            }, 300)
         })
     }
 
